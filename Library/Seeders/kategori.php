f6c61321154a0ed93552fcd44efdce22fbfc4d6e
alfajri15 <37392389+alfajri15@users.noreply.github.com>
2023-03-14 08:53:41 +0700

    Create kategori.php

---
+++ Library/Seeders/kategori.php
@@ -0,0 +1,12 @@
+DB::table('kategori')->insert(
+        [
+            [
+                'nama' => 'Sejarah'
+            ],
+            [
+                'nama' => 'Sosial & Politik'
+            ],
+            [
+                'nama' => 'Pertanian'
+            ],
+        ]);
