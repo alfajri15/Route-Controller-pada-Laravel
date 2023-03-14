b9e3da74537de81f59cb97b92789314a714f6a61
alfajri15 <37392389+alfajri15@users.noreply.github.com>
2023-03-14 08:52:55 +0700

    Create pengarang.php

---
+++ Library/Seeders/pengarang.php
@@ -0,0 +1,12 @@
+DB::table('pengarang')->insert(
+        [
+            [
+                'nama' => 'Deden Hamdani','email' => 'deden@gmail.com','hp' => '0857123456'
+            ],
+            [
+                'nama' => 'Dedi Iskandar','email' => 'dedi@gmail.com','hp' => '0857789456'
+            ],
+            [
+                'nama' => 'Siti Aminah','email' => 'siti@gmail.com','hp' => '0813789456'
+            ]
+        ]);
