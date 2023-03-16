7b137d2c8c871fb9498b569f92daa237f21169bf
alfajri15 <37392389+alfajri15@users.noreply.github.com>
2023-03-16 11:24:26 +0700

    Update PengarangController.php

--- Library/Controllers/PengarangController.php
+++ Library/Controllers/PengarangController.php
@@ -25,7 +25,8 @@ class PengarangController extends Controller
      */
     public function create()
     {
-        //
+        //mengarahkan ke halaman form input
+        return view('pengarang.form');
     }
 
     /**
@@ -33,7 +34,27 @@ class PengarangController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        //proses upload foto
+        if(!empty($request->foto)){
+            $request->validate([
+            'foto' => 'image|mimes:jpg,jpeg,png,giff|max:2048',
+            ]);
+            $fileName = $request->nama.'.'.$request->foto->extension();
+            $request->foto->move(public_path('images'), $fileName);
+            }else{
+            $fileName = '';
+            }
+
+        DB::table('pengarang')->insert(
+            [
+                'nama' => $request->nama,
+                'email' => $request->email,
+                'hp' => $request->hp,
+                'foto' => $fileName,
+            ]
+        );
+        //landing page
+        return redirect('/pengarang');
     }
 
     /**
@@ -41,7 +62,10 @@ class PengarangController extends Controller
      */
     public function show(string $id)
     {
-        //
+        //menampilkan detail pengarang
+        $ar_pengarang = DB::table('pengarang')
+                        ->where('id','=',$id)->get();
+        return view('pengarang.show',compact('ar_pengarang'));    
     }
 
     /**
@@ -49,7 +73,10 @@ class PengarangController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        //mengarahkan ke halaman form edit
+        $data = DB::table('pengarang')
+            ->where('id','=',$id)->get();
+        return view('pengarang.form_edit',compact('data'));
     }
 
     /**
@@ -57,7 +84,17 @@ class PengarangController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        //proses edit data lama, tangkap request dari form edit pengarang
+        DB::table('pengarang')->where('id','=',$id)->update(
+            [
+                'nama' => $request -> nama,
+                'email' => $request -> email,
+                'hp' => $request -> hp,
+                'foto' => $request -> foto
+            ]
+        );
+        //Landing Page
+        return redirect('/pengarang'.'/'.$id);
     }
 
     /**
@@ -65,6 +102,8 @@ class PengarangController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        //menghapus data
+        DB::table('pengarang')->where('id',$id)->delete();
+        return redirect('/pengarang');
     }
 }
